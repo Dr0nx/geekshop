6e001aee2c1406e99bbc105badbf759112ae2458
Andrey Bozhkov <kovbozh@gmail.com>
2021-12-02 14:46:19 +0300

    edit views

--- authapp/views.py
+++ authapp/views.py
@@ -17,8 +17,8 @@ def login(request):
             if user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
-        # else:
-        #     print(form.errors)
+        else:
+            print(form.errors)
     else:
         form = UserLoginForm()
     context = {
@@ -41,7 +41,8 @@ def register(request):
         form = UserRegisterForm()
     context = {
         'title': 'Geekshop | Регистрация',
-        'form': form}
+        'form': form
+    }
     return render(request, 'authapp/register.html', context)
 
 
@@ -51,13 +52,22 @@ def profile(request):
         form = UserProfilerForm(instance=request.user, data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
+            messages.set_level(request, messages.SUCCESS)
+            messages.success(request, 'Вы успешно сохранили профайл')
         else:
-            print(form.errors)
+            messages.set_level(request, messages.ERROR)
+            messages.error(request, form.errors)
+
+    baskets = Basket.objects.filter(user=request.user)
+    total_sum = sum(basket.sum() for basket in baskets)
+    total_quantity = sum(basket.quantity for basket in baskets)
 
     context = {
         'title': 'Geekshop | Профайл',
         'form': UserProfilerForm(instance=request.user),
-        'baskets': Basket.objects.filter(user=request.user)
+        'baskets': Basket.objects.filter(user=request.user),
+        'total_quantity': total_quantity,
+        'total_sum': total_sum
     }
     return render(request, 'authapp/profile.html', context)
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
 # import os.path
 from django.shortcuts import render
+from django.views.generic import DetailView
+
 from mainapp.models import ProductCategory, Product
 
 
@@ -19,7 +21,24 @@ def products(request):
         'products': Product.objects.all(),
     }
 
-#    with open("mainapp/fixtures/products.json", "w") as write_file:
-#        json.dump(context, write_file)
+    #    with open("mainapp/fixtures/products.json", "w") as write_file:
+    #        json.dump(context, write_file)
 
     return render(request, 'mainapp/products.html', context)
+
+
+class ProductDetail(DetailView):
+    """
+    Контроллер вывода информации о продукте
+    """
+    model = Product
+    template_name = 'mainapp/detail.html'
+
+    # context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        product = self.get_object()
+        context['product'] = product
+        context['categories'] = ProductCategory.objects.all()
+        return context
