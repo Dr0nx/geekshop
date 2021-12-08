da0842dd45957c5f79cc7736647e76ec45d50c83
Andrey Bozhkov <kovbozh@gmail.com>
2021-12-08 14:11:01 +0300

    Push all lesson 7

---
+++ admins/forms.py
@@ -0,0 +1,77 @@
+from django import forms
+
+from authapp.forms import UserRegisterForm, UserProfilerForm
+from authapp.models import User
+from mainapp.models import ProductCategory, Product
+
+
+class UserAdminRegisterForm(UserRegisterForm):
+    image = forms.ImageField(widget=forms.FileInput(), required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'image', 'first_name', 'last_name', 'age', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserAdminRegisterForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class UserAdminProfileForm(UserProfilerForm):
+    email = forms.EmailField(widget=forms.EmailInput())
+    username = forms.CharField(widget=forms.TextInput())
+
+    def __init__(self, *args, **kwargs):
+        super(UserAdminProfileForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['readonly'] = False
+        self.fields['username'].widget.attrs['readonly'] = False
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class ProductCategoryAdminForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput())
+    description = forms.CharField(widget=forms.TextInput())
+
+    class Meta:
+        model = ProductCategory
+        fields = ('name', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super(ProductCategoryAdminForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+
+class ProductAdminForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput())
+    short_description = forms.CharField(widget=forms.TextInput())
+    category = forms.ModelChoiceField(empty_label='Выберите категорию', queryset=ProductCategory.objects.all(),
+                                      widget=forms.Select())
+    price = forms.CharField(widget=forms.TextInput())
+    quantity = forms.CharField(widget=forms.TextInput())
+    image = forms.ImageField(widget=forms.FileInput(), required=False)
+
+    class Meta:
+        model = Product
+        fields = ('name', 'short_description', 'category', 'price', 'quantity', 'image')
+
+    def __init__(self, *args, **kwargs):
+        super(ProductAdminForm, self).__init__(*args, **kwargs)
+
+        # for field_name, field in self.fields.items():
+        #     field.widget.attrs['class'] = 'form-control py-4'
+        # self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+        for field_name, field in self.fields.items():
+            if field_name == 'image' or field_name == 'category':
+                field.widget.attrs['class'] = 'form-control'
+            else:
+                field.widget.attrs['class'] = 'form-control py-4'
+        self.fields['image'].widget.attrs['class'] = 'custom-file-input'
---
+++ admins/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from admins.views import index, admin_users, admin_users_create, admin_users_update, admin_users_delete, \
+    admin_category, admin_category_create, admin_category_update, admin_category_delete, admin_product, \
+    admin_product_create, admin_product_delete, admin_product_update
+
+app_name = 'admins'
+urlpatterns = [
+    path('', index, name='index'),
+    path('users/', admin_users, name='admin_users'),
+    path('users-create/', admin_users_create, name='admin_users_create'),
+    path('users-update/<int:pk>', admin_users_update, name='admin_users_update'),
+    path('users-delete/<int:pk>', admin_users_delete, name='admin_users_delete'),
+    path('category/', admin_category, name='admin_category'),
+    path('category/create/', admin_category_create, name='admin_category_create'),
+    path('category-delete/<int:pk>/', admin_category_delete, name='admin_category_delete'),
+    path('category-update/<int:pk>/', admin_category_update, name='admin_category_update'),
+    path('product/', admin_product, name='admin_product'),
+    path('product/create/', admin_product_create, name='admin_product_create'),
+    path('product-delete/<int:pk>/', admin_product_delete, name='admin_product_delete'),
+    path('product-update/<int:pk>/', admin_product_update, name='admin_product_update'),
+]
