db5bb40f9c0a40c71ea69d261ccc312f625b5dcb
Andrey Bozhkov <kovbozh@gmail.com>
2021-12-09 10:57:56 +0300

    edit admin.py

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from authapp.models import User
+from baskets.admin import BasketAdmin
+from baskets.models import Basket
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    model = Basket
+    inlines = (BasketAdmin,)
