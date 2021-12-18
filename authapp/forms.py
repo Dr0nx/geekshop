2ff2400533b0034c88c9aeea9fc830272bbf8bab
Andrey Bozhkov <kovbozh@gmail.com>
2021-12-18 21:09:24 +0300

    edit forms.py, login.html, models.py

--- authapp/forms.py
+++ authapp/forms.py
@@ -4,7 +4,7 @@ import random
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 # from django.core.exceptions import ValidationError
 from django import forms
-from authapp.models import User
+from authapp.models import User, UserProfile
 from authapp.validator import validate_name
 
 
@@ -71,3 +71,19 @@ class UserProfilerForm(UserChangeForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class UserProfileEditForm(forms.ModelForm):
+    # bdate = forms.CharField(widget=forms.TextInput())
+
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != 'gender':
+                field.widget.attrs['class'] = 'form-control py-4'
+            else:
+                field.widget.attrs['class'] = 'form-control'
--- authapp/models.py
+++ authapp/models.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.timezone import now
 
 
@@ -16,3 +18,27 @@ class User(AbstractUser):
         if now() <= self.activation_key_expires + timedelta(hours=48):
             return False
         return True
+
+
+class UserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж'),
+    )
+
+    user = models.OneToOneField(User, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+    about = models.TextField(verbose_name='про меня', blank=True, null=True)
+    gender = models.CharField(verbose_name='пол', choices=GENDER_CHOICES, blank=True, max_length=1)
+    bdate = models.CharField(verbose_name='дата рождения', max_length=8)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
