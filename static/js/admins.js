499ff3bba21fde6997485b44b5d9a4ca81df08ac
Andrey Bozhkov <kovbozh@gmail.com>
2021-12-23 22:39:00 +0300

    add ajax

---
+++ static/js/admins.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    $('.ajax_change select').change(function () {
+        let lang = document.getElementById("language_site")
+        lang.type = "submit";
+        $('#language_site').trigger('click');
+        lang.type = "hidden";
+    });
+}
