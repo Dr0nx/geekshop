63d2a8928b4390ce1297fc6b086055ff2dd00c81
Andrey Bozhkov <kovbozh@gmail.com>
2021-12-23 22:44:22 +0300

    add jquery.js

--- static/vendor/jquery/jquery.js
+++ static/vendor/jquery/jquery.js
@@ -10387,7 +10387,7 @@ jQuery.fn.load = function( url, params, callback ) {
 
 			self.html( selector ?
 
-				// If a selector was specified, locate the right elements in a dummy div
+				// If a selector was specified, locale the right elements in a dummy div
 				// Exclude scripts to avoid IE 'Permission Denied' errors
 				jQuery( "<div>" ).append( jQuery.parseHTML( responseText ) ).find( selector ) :
 
