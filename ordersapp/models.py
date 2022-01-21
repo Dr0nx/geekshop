e19f51a421ce68a4be4ba76487530ae05473f499
Andrey Bozhkov <kovbozh@gmail.com>
2022-01-21 20:14:41 +0300

    edit models.py

--- ordersapp/models.py
+++ ordersapp/models.py
@@ -23,8 +23,8 @@ class Order(models.Model):
     )
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    create = models.DateTimeField(verbose_name='создан', auto_now_add=True)
-    update = models.DateTimeField(verbose_name='обновлен', auto_now=True)
+    created = models.DateTimeField(verbose_name='создан', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='обновлен', auto_now=True)
     status = models.CharField(choices=ORDER_STATUS_CHOICES, verbose_name='статус', max_length=3, default=FORMING)
     is_active = models.BooleanField(verbose_name='активный', default=True, )
 
@@ -48,13 +48,14 @@ class Order(models.Model):
             item.save()
         self.is_active = False
         self.save()
-    
+
     def get_summary(self):
         items = self.orderitems.select_related()
         return {
             'get_total_cost': sum(list(map(lambda x: x.get_product_cost(), items))),
             'get_total_quantity': sum(list(map(lambda x: x.quantity, items)))
         }
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, verbose_name='заказ', related_name='orderitems', on_delete=models.CASCADE)
